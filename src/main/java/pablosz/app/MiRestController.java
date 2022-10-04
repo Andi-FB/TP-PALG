356726798cace8910f4e1164572288704d62da69
andi-fb <andres.fernandezbina@mercadolibre.com>
2022-10-03 21:13:26 -0300

    second first commit :P

---
+++ src/main/java/pablosz/app/MiRestController.java
@@ -0,0 +1,29 @@
+package pablosz.app;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pablosz.app.domain.Jugador;
+
+@RestController
+public class MiRestController
+{
+	@RequestMapping("/hola/{name}")
+	public String test(@PathVariable String name) {
+		return "Hola Mundo, Hola " + name + " !";
+	}
+
+	@PostMapping("/hola")
+	public String test2(@RequestBody Jugador jugador){
+		return "Hola Jugador: " + jugador.getNombre() + " Numero de camiseta: " + jugador.getId() + " jugas de: " + jugador.getPosicion();
+
+	}
+	@RequestMapping("/hola")
+	public String test() {
+		return "Hola Mundo!";
+	}
+}
---
+++ src/main/java/pablosz/app/domain/Jugador.java
@@ -0,0 +1,43 @@
+package pablosz.app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Jugador {
+
+    @Id
+    private Integer id;
+
+    private String nombre;
+
+    private String posicion;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+
+}
