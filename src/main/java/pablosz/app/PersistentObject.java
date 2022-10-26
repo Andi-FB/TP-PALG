16ecd10e202bb56006484ed5b57a2a5a09aa03b0
Ruben Cardenas <rbn_125@hotmail.com>
2022-10-26 20:42:53 -0300

    Modificaciones iniciales

--- src/main/java/pablosz/app/PersistentObject.java
+++ src/main/java/pablosz/app/PersistentObject.java
@@ -3,11 +3,15 @@ package pablosz.app;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import pablosz.app.domain.Jugador;
 import pablosz.app.domain.MySession;
 import pablosz.app.domain.PersistentObjectDTO;
 import javax.persistence.EntityManager;
+import java.util.List;
 
+@Repository
 public class PersistentObject {
 
     private Gson gson = new GsonBuilder().create();
@@ -15,6 +19,37 @@ public class PersistentObject {
     @Autowired
     private EntityManager em;
 
+    public void createSession(long key, int ttl) {
+        MySession session = new MySession(key, ttl);
+        em.persist(session);
+    }
+
+    public void store(long key, Object object) {
+        MySession entity = (MySession) em.createQuery("SELECT t FROM MySession t where t.key = :value1")
+                .setParameter("value1", key).getSingleResult();
+        entity.addParameter(object);
+    }
+
+    public void destroySession(long key) {
+        MySession entity = (MySession) em.createQuery("SELECT t FROM MySession t where t.key = :value1")
+                .setParameter("value1", key).getSingleResult();
+        em.remove(entity);
+    }
+
+    public Object load(long key, Class clazz) {
+        MySession entity = (MySession) em.createQuery("SELECT t FROM MySession t where t.key = :value1")
+                .setParameter("value1", key).getSingleResult();
+        return entity.getParameters().stream().filter(x -> x.getClass() == clazz).findFirst().get();
+    }
+
+    public void remove(long key, Class clazz) {
+        MySession entity = (MySession) em.createQuery("SELECT t FROM MySession t where t.key = :value1")
+                .setParameter("value1", key).getSingleResult();
+
+        Object parameter = entity.getParameters().stream().filter(x -> x.getClass() == clazz).findFirst().get();
+        entity.getParameters().remove(parameter);
+    }
+/*
     public MySession getSession(){
        return new MySession(1, 1);
     }
@@ -42,5 +77,5 @@ public class PersistentObject {
     public void destroySession(long key){
 
     }
-
+*/
 }
--- src/main/java/pablosz/app/domain/MySession.java
+++ src/main/java/pablosz/app/domain/MySession.java
@@ -3,14 +3,20 @@ package pablosz.app.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity()
 public class MySession {
-
+    @Id
     @Column(name="key")
     private long key;
     @Column(name="ttl")
     private int ttl; //time to live
+    @Transient
+    private List<Object> parameters;
 
 
     public MySession(long key, int ttl){
@@ -18,4 +24,38 @@ public class MySession {
         this.ttl = ttl;
     }
 
+    public MySession() {
+
+    }
+
+    public long getKey() {
+        return key;
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(int ttl) {
+        this.ttl = ttl;
+    }
+
+    public List<Object> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Object> parameters) {
+        this.parameters = parameters;
+    }
+
+    public void addParameter(Object parameter) {
+        if(this.parameters == null){
+            this.parameters = new ArrayList<>();
+        }
+        this.parameters.add(parameter);
+    }
 }
